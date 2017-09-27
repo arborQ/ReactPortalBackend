2439893714fe804eb9f75110798ebb41a0402008
Łukasz Wójcik <arbor@o2.pl>
2017-09-27 23:50:31 +0200

    lol

--- ReactPortalApi/Controllers/ValuesController.cs
+++ ReactPortalApi/Controllers/ValuesController.cs
@@ -1,11 +1,13 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ReactPortalApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
